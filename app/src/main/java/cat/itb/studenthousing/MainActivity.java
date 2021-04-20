ab0782f7fdd43b0eaba1085a3af7caa9e20fbe0e
sergioparamo <sergio.paramo1997@gmail.com>
2021-04-20 17:07:48 +0200

    added first classes and xml layouts

--- app/src/main/java/cat/itb/studenthousing/MainActivity.java
+++ app/src/main/java/cat/itb/studenthousing/MainActivity.java
@@ -1,8 +1,11 @@
 package cat.itb.studenthousing;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+
+
     }
 }
